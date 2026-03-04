e04a1eb64766e2a4afb586f0c222b7432ff1ed36
Kavishka Rasanjana <krasanjana81@gmail.com>
2026-03-04 14:49:25 +0530

    Initial commit: Spring Boot + React CRUD

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="api/v1/")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
+
+    }
+
+    @PostMapping("/adduser")
+    public  UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteuser")
+    public  String deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
+
+
+}
+
